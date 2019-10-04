13f6f8fe7e24889953b8b4e0ae699972701573b7
jan.de.deurwaerder@howest.be <jan.de.deurwaerder@howest.be>
2019-10-04 10:09:07 +0200

    Add project files.

---
+++ oe_selectie_03.wpf/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace oe_selectie_03.wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        void Initialiseer()
+        {
+        }
+        private void TxtWetenschappen_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(txtWetenschappen.IsLoaded)
+            {
+            }
+        }
+
+        private void TxtWiskunde_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(txtWiskunde.IsLoaded)
+            {
+            }
+        }
+
+        private void TxtTalen_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(txtTalen.IsLoaded)
+            {
+            }
+        }
+
+        private void CmbWetenschappen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbWetenschappen.IsLoaded)
+            {
+            }
+        }
+
+        private void CmbWiskunde_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbWetenschappen.IsLoaded)
+            {
+            }
+        }
+
+        private void CmbTalen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbWetenschappen.IsLoaded)
+            {
+            }
+        }
+ 
+        private void TxtWetenschappen_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtWetenschappen.SelectAll();
+        }
+
+        private void TxtWiskunde_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtWiskunde.SelectAll();
+        }
+
+        private void TxtTalen_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtTalen.SelectAll();
+        }
+    }
+}
